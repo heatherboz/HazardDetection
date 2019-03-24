b94e8a0e58e3e56c0437345f4437cd9cd059317e
heatherboz <33397779+heatherboz@users.noreply.github.com>
2019-03-24 18:59:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,6 @@ slowThreshold = sensorHeight/(np.cos(sensorAngle)) + buffer # slow down if we ma
 
 mathvalue = (sensorHeight*np.tan(sensorAngle) - collisionThreshold)*np.sin(rampAngle) # see diagram
 stopThreshold = slowThreshold + mathvalue/np.cos(rampAngle + sensorAngle) + buffer # stop if not a ramp
-
-# output 3 ints: 0 (T), 1 (F), 2 (S)
-def detection(left, right, front, threshold, slow_stop, original_values): 
-    l_out, r_out, f_out = original_values
-    if left >= threshold: l_out = slow_stop
-    if right >= threshold: r_out = slow_stop 
-    if front >= threshold: f_out = slow_stop
-    return (l_out, r_out,  f_out) 
    
 def collisionDetection(left_forw, right_forw, front_forw): # assume these are distances 
     l_out, r_out, f_out = 0,0,0
@@ -28,15 +20,16 @@ def collisionDetection(left_forw, right_forw, front_forw): # assume these are di
     return (l_out, r_out,  f_out) 
 #    return detection(left_forw, right_forw, front_forw, collisionThreshold, 1, (0,0,0))
 
-
-def stairDetection(left_tilt, right_tilt, front_tilt): 
-    slow_vals = detection(left_tilt, right_tilt, front_tilt, slowThreshold, 2, (0,0,0))
-    print(slow_vals)
-    return detection(left_tilt, right_tilt, front_tilt, stopThreshold, 1, slow_vals)
+# returns 2 if need to slow, 1 if need to stop
+def stairDetection(front_tilt):
+    if front_tilt >= stopThreshold:
+        return 1;
+    if front_tilt >= slowThreshold:
+        return 2;
 
 if __name__ == "__main__":
     l, r, f, l_t, r_t, f_t = .5, .5, .5, 2, 2, 2
     print(collisionDetection(l, r, f))
-    print(stairDetection(l_t, r_t, f_t))
+    print(stairDetection(f_t))
     
     
